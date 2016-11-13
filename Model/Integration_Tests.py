06aa0d58144d4bd62c7cc88cd25c437b861319e9
lt1503 <leedtan@gmail.com>
2016-11-13 18:03:09 -0500

    added sine test

--- Model/Integration_Tests.py
+++ Model/Integration_Tests.py
@@ -19,7 +19,7 @@ def test_xor(verb=0):
     y_predict = NN.predict(X)
     print(y_predict)
     return np.mean(np.square(y_predict - Y))
-if (1):
+if (0):
     np.random.seed(1)
     xor_err = test_xor()
     print('xor error = ', xor_err)
@@ -42,7 +42,29 @@ def test_addition(verb=0):
     y_predict = NN.predict(X)
     print(y_predict)
     return np.mean(np.square(y_predict - Y))
-if (1):
+if (0):
     np.random.seed(1)
     add_error = test_addition()
     print('add error = ', add_error)
+
+
+def test_sine(verb=1):
+    NN = NeuralNetworkUI([1, 2000, 1], trans_fcns="tanh", reg_const=1e-6)
+    X = np.linspace(0, 2*np.pi, num=5)
+    Y = np.array([np.sin(x) for x in X])
+    if np.ndim(X) == 1:
+        X = np.atleast_2d(X).T
+        Y = np.atleast_2d(Y).T
+    print(Y.T)
+    for _ in range(10):
+        NN.train(X, Y, epochs=100)
+        if verb > 0:
+            y_predict = NN.predict(X)
+            print(y_predict)
+    y_predict = NN.predict(X)
+    print(y_predict)
+    return np.mean(np.square(y_predict - Y))
+if (1):
+    np.random.seed(1)
+    sine_error = test_sine()
+    print('sine error = ', sine_error)
