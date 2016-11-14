63cdb1530990563153ea75b0ea24471a7916fd8d
lt1503 <leedtan@gmail.com>
2016-11-13 19:54:00 -0500

    tried to implement an auto encoder. May be working, but need to measure correlation between input and output, allowing for reverse correlation

--- Model/Integration_Tests.py
+++ Model/Integration_Tests.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from Model import utils
 from Model.Network.NeuralNetworkUI import NeuralNetworkUI
+from Model.Network.AutoEncoder import AutoEncoder
 
 
 def test_xor(verb=0):
@@ -64,7 +65,38 @@ def test_sine(verb=1):
     y_predict = NN.predict(X)
     print(y_predict)
     return np.mean(np.square(y_predict - Y))
-if (1):
+if (0):
     np.random.seed(1)
     sine_error = test_sine()
     print('sine error = ', sine_error)
+
+def test_encoding(verb=0):
+    X1h = np.random.rand(20)
+    X2h = np.random.rand(20)
+    X = np.zeros([20, 5])
+    for idx in range(20):
+        X1 = X1h[idx]
+        X2 = X2h[idx]
+        X[idx,:] = [X1*X2, X1+X2, X1/(1+X2), X2/(1+X1), X1-X2]
+    X = X/2
+    X1h = np.atleast_2d(X1h)
+    X2h = np.atleast_2d(X2h)
+    encoder = AutoEncoder(X)
+    encoding_vals = encoder.get_encoding_vals(X)
+    for _ in range(10):
+        encoder.train(X, 100)
+        encoding_vals = encoder.get_encoding_vals(X)
+        reconstruction = np.round(encoder.predict(X), 3)
+        #print("reconstruction error: ", np.mean(np.square(reconstruction - X)))
+        err1 = np.mean(np.square(np.concatenate([X1h, X2h]).T - encoding_vals))
+        err2 = np.mean(np.square(np.concatenate([X2h, X1h]).T - encoding_vals))
+        print(err1, err2)
+        #print("encoding error: ", np.min([err1, err2]))
+        #print("encoding_vals:   ", encoding_vals)
+        #print(np.mean(np.square(reconstruction - X)))
+    print(X)
+    return np.mean(np.square(reconstruction - X))
+if (1):
+    np.random.seed(1)
+    encoding_error = test_encoding()
+    print('autoencoding error = ', encoding_error)
--- Model/Network/AutoEncoder.py
+++ Model/Network/AutoEncoder.py
@@ -1,9 +1,10 @@
 
+import numpy as np
 from Model import utils
 from Model.Network.Network import Network
 from Model.Network.NeuralNetworkLearner import NeuralNetworkLearner
 
-class NeuralNetworkUI(object):
+class AutoEncoder(object):
     '''
     This is the User Inferface for a user to create a neural network.
     A user can create the network, and then train the network for
@@ -15,18 +16,26 @@ class NeuralNetworkUI(object):
     Visualization description?
     '''
 
-    def __init__(self, layers, trans_fcns='sigmoid', loss_fcn='mse', reg_const = 1e-3,
-                 learn_alg = utils.MOMENTUM_BP, learning_rate = 1e-3):
+    def __init__(self, X):
         #MAKE THIS LOOK LIKE A PRIVATE VAREIABLE
-        self.network = Network(layers, trans_fcns, loss_fcn, reg_const)
+        self.network = Network(layer_sizes = [X.shape[1], 100, 2, 100, X.shape[1]],
+                               trans_fcns="tanh", reg_const=1e-5)
         self.neuralnetworklearner = \
-            NeuralNetworkLearner(network = self.network, learning_rate = learning_rate)
+            NeuralNetworkLearner(network = self.network)
+        self.train(X, 1)
 
-    def train(self, X, Y, epochs = 10):
-        self.neuralnetworklearner.run_epochs(X, Y, epochs)
+    def train(self, X, epochs = 10):
+        self.neuralnetworklearner.run_epochs(X, X, epochs)
+
+    def get_encoding_vals(self, X):
+        hidden_repr = np.zeros([X.shape[0], 2])
+        for x_idx, xi in enumerate(X):
+            self.network.feed_forward(xi)
+            hidden_repr[x_idx] = self.network.layers[1].act_vals
+        return hidden_repr
 
     def predict(self, X):
-        return [self.network.predict(xi) for xi in X]
+        return np.array([self.network.predict(xi) for xi in X])
 
     @property
     def network(self):
