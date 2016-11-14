668f7100e01cf689edca8f940c0e4502e247d13e
lt1503 <leedtan@gmail.com>
2016-11-14 08:52:08 -0500

    regression and categorical autoencoders seem to be working

--- Model/Integration_Tests.py
+++ Model/Integration_Tests.py
@@ -70,7 +70,7 @@ if (0):
     sine_error = test_sine()
     print('sine error = ', sine_error)
 
-def test_encoding(verb=0):
+def test_encoding_regression(verb=0):
     num_x = 100
     X1h = np.random.rand(num_x)
     X2h = np.random.rand(num_x)
@@ -94,7 +94,36 @@ def test_encoding(verb=0):
             print(err1, err2)
         reconstruction = encoder.predict(X)
     return np.mean(np.square(reconstruction - X))
+if (0):
+    np.random.seed(1)
+    encoding_regression_error = test_encoding_regression(1)
+    print('autoencoding regression error = ', encoding_regression_error)
+
+def test_encoding_categorical(verb=0):
+    num_x = 100
+    X1h = np.random.randint(0,2,num_x)
+    X2h = np.random.randint(0,2,num_x)
+    size_encoding = 6
+    X = np.zeros([num_x, size_encoding])
+    for idx in range(num_x):
+        X1 = X1h[idx]
+        X2 = X2h[idx]
+        X[idx,:] = [X1, X2,X1 + X2, X1 - X2, X2 - X1, X1*X2]
+    X = X/4 + np.random.rand(num_x, size_encoding)/100
+    encoder = AutoEncoder(X)
+    encoding_vals = encoder.get_encoding_vals(X)
+    for _ in range(10):
+        encoder.train(X, 10)
+        if verb > 0:
+            encoding_vals = encoder.get_encoding_vals(X)
+            err1 = np.abs(np.corrcoef(x=X1h, y=encoding_vals[:,0])[0,1]) + \
+                   np.abs(np.corrcoef(x=X2h, y=encoding_vals[:,1])[0,1])
+            err2 = np.abs(np.corrcoef(x=X2h, y=encoding_vals[:,0])[0,1]) + \
+                   np.abs(np.corrcoef(x=X1h, y=encoding_vals[:,1])[0,1])
+            print("correlation between true hidden and encoders: ", np.max([err1, err2])/2)
+        reconstruction = encoder.predict(X)
+    return np.mean(np.square(reconstruction - X))
 if (1):
     np.random.seed(1)
-    encoding_error = test_encoding(1)
-    print('autoencoding error = ', encoding_error)
+    encoding_categorical_error = test_encoding_categorical(1)
+    print('autoencoding regression error = ', encoding_categorical_error)
