24ab0697f02404527aceb8fff232a71f558c516f
lt1503 <leedtan@gmail.com>
2016-11-27 12:02:24 -0500

    working on testing code

--- Model/FeedForwardNetApps/tests/test_autoencode_data.py
+++ Model/FeedForwardNetApps/tests/test_autoencode_data.py
@@ -8,7 +8,6 @@ from Model.FeedForwardNetApps import *
 from Model.FeedForwardNetwork.NetworkLayers import *
 
 
-
 from dataprep.loader import *
 from dataprep.transformer import *
 
@@ -17,34 +16,61 @@ def process_arrhythmia():
     csvloader = CSVLoader(target=[-1])
     datasetX, datasetY = csvloader.load_from_path('dataset/arrhythmia.data')
 
+    tsX = TabularSchemaTransformer(
+            [
+                "numeric",          # Age
+                "categorical",      # Sex
+                "numeric",          # Height
+                "numeric",          # Weight
+                "numeric",          # QRS duration
+                "numeric",          # P-R interval
+                "numeric",          # Q-T interval
+                "numeric",          # T interval
+                "numeric",          # P interval
+            ] +
+            ["numeric"] * 12 +      # 10-21
+            ["categorical"] * 6 +   # 22-27
+            ["numeric"] * 252       # 28-279
+            )
+    tsY = TabularSchemaTransformer(["categorical"])
+
+    # Normalize all numeric columns into [-1, 1] first.
+    norm = Normalizer('negpos')
+    # Replace all string values equal to '?' to NaN
+    # Note that the '?''s in numeric fields are already replaced by NaNs
+    # in TabularSchemaTransformer
     nv = NullValueTransformer('?')
-
+    # Add a 'missing indicator' for each column with missing values
     ni = NullIndicatorTransformer()
-
+    # Impute the NaN's in numeric fields by mean value
     mi = NumericImputationTransformer('mean')
-
+    # One-hot categorical encoder.  PD.get_dummies() does all the magic
     ohe = OneHotEncoderTransformer()
 
-    pipeline = PipelineTransformer(nv, ni, mi, ohe)
+    # A pipeline which does transformation one-by-one
+    pipelineX = PipelineTransformer(tsX, norm, nv, ni, mi, ohe)
+    # For labels we don't need to normalize them
+    pipelineY = PipelineTransformer(tsY, nv, ni, mi, ohe)
 
-    processedX = pipeline.transform(datasetX)
-    processedY = pipeline.transform(datasetY)
-
+    # Transform dataset
+    processedX = pipelineX.transform(datasetX)
+    processedY = pipelineY.transform(datasetY)
     return processedX, processedY
-
 
 
 def test_arrhythmia(d = 3, visualize=1):
 
     X, Y = process_arrhythmia()
+    print(Y)
 
     num_x = X.shape[0]
     print(X.shape)
     color_list = list(six.iteritems(colors.cnames))
     print(Y[:,0])
-    rgb = [color_list[y[0]*5][0] for y in Y]
+    rgb = [color_list[np.sum([yi*2 for yi in y])][0] for y in Y]
     encoder = AutoEncoder(X, hidden_dim=3)
-    print(X)
+    print(np.mean(X, 0))
+    return
     for _ in range(10):
         encoder.train(1)
         reconstruction = encoder.predict()
