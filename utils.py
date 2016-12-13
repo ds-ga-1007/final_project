07a5606ec96c6a24b608c92722c7e2696aaa10d4
lt1503 <leedtan@gmail.com>
2016-12-12 20:11:16 -0500

    renamed rgb to data_color_labels

--- utils.py
+++ utils.py
@@ -10,32 +10,32 @@ def prepare_autoencoding_data(X, Y):
     prepare data to be autoencoded
     :param X: numpy.ndarray 2D array of independent high dimensional variables to be autoencoded
     :param Y: numpy.ndarray 1D array of response variables for coloring purposes
-    :return: X = numpy.ndarray of processed input array, rgb = list of color labels.
+    :return: X = numpy.ndarray of processed input array, data_color_labels = list of color labels.
     """
 
     color_list = list(six.iteritems(colors.cnames))
 
     if Y is None:
-        rgb = [color_list[0][0] for x in X]
+        data_color_labels = [color_list[0][0] for x in X]
 
     elif Y.ndim == 1:
-        rgb = [color_list[y * 2 + 1][0] for y in Y]
+        data_color_labels = [color_list[y * 2 + 1][0] for y in Y]
 
     elif Y.ndim == 2:
-        rgb = [color_list[4 * np.sum(
+        data_color_labels = [color_list[4 * np.sum(
             [yi * (2 ** idx) for idx, yi in enumerate(y)])][0] for y in Y]
 
     else:
         raise ValueError("response variables must be 1 or 2 dimensional")
 
-    return X, rgb
+    return X, data_color_labels
 
-def visualize_autoencoding_data(encoder, d, rgb, fig):
+def visualize_autoencoding_data(encoder, d, data_color_labels, fig):
     """
     Visualize the autoencoding
     :param encoder: AutoEncoder to visualize
     :param d: dimension of data to be AutoEncoded
-    :param rgb: Array of color names for the autoencoding visualizations points
+    :param data_color_labels: Array of color names for the autoencoding visualizations points
     :param fig: matplotlib.figure.Figure to draw the visualization
     :return: None
     """
@@ -44,20 +44,20 @@ def visualize_autoencoding_data(encoder, d, rgb, fig):
 
     if d == 2:
         fig.add_subplot(120 + d - 1)
-        plt.scatter(encoding_vals[:, 0], encoding_vals[:, 1], color=rgb)
+        plt.scatter(encoding_vals[:, 0], encoding_vals[:, 1], color=data_color_labels)
     if d == 3:
         ax = fig.add_subplot(120 + d - 1, projection='3d')
         ax.scatter(xs=encoding_vals[:, 0], ys=encoding_vals[:, 1],
-                   zs=encoding_vals[:, 2], c=rgb)
+                   zs=encoding_vals[:, 2], c=data_color_labels)
 
     plt.title('real data visualized with graphed in ' + str(d) + 'D')
 
-def process_autoencoding_data(X, visualize, rgb):
+def process_autoencoding_data(X, visualize, data_color_labels):
     """
-    Perform autoencoding of data. Optionally visualize data using color tags from rgb.
+    Perform autoencoding of data. Optionally visualize data using color tags from data_color_labels.
     :param X: numpy.ndarray 2D array of independent high dimensional variables to be autoencoded.
     :param visualize: Boolean-like. If evaluates to True, display visualizations.
-    :param rgb: Array of color names for the autoencoding visualizations points.
+    :param data_color_labels: Array of color names for the autoencoding visualizations points.
     :return: array of reconstruction errors for an embedding dimension of width 2 or 3.
     """
 
@@ -78,7 +78,7 @@ def process_autoencoding_data(X, visualize, rgb):
         reconstruction = encoder.predict()
 
         if visualize:
-            visualize_autoencoding_data(encoder, d, rgb, fig)
+            visualize_autoencoding_data(encoder, d, data_color_labels, fig)
 
         err[d-2] = np.mean(np.square(reconstruction - X))
 
@@ -97,8 +97,8 @@ def autoencode_2d_3d_data(X, Y, visualize = 0):
     :return: array of reconstruction errors for an embedding dimension of width 2 or 3.
     """
 
-    X, rgb = prepare_autoencoding_data(X, Y)
+    X, data_color_labels = prepare_autoencoding_data(X, Y)
 
-    err = process_autoencoding_data(X, visualize, rgb)
+    err = process_autoencoding_data(X, visualize, data_color_labels)
 
     return err[0], err[1]
