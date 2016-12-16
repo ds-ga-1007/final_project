085a3aebe5e9a07f0bd089fc64d52d3cb1bb9343
Hezhi Wang <hw1567@nyu.edu>
2016-12-15 22:06:54 -0500

    make some changes of figure size

--- hz1411-hw1567-sar516-master/Overview/overview_plot.py
+++ hz1411-hw1567-sar516-master/Overview/overview_plot.py
@@ -31,7 +31,7 @@ class overview_plot:
         mean = np.mean(reviews)
         std = np.std(reviews)
         median = np.median(reviews)
-        plt.figure(figsize=(15,15))
+        plt.figure(figsize=(12,12))
         ax = reviews.plot(kind='kde')
         ax.set_xlim([0,max(reviews)])
         text = 'mean=%.2f \n std=%.2f \n median=%.2f'%(mean, std, median)
@@ -62,7 +62,7 @@ class overview_plot:
         mean = np.mean(rating)
         std = np.std(rating)
         median = np.median(rating)
-        plt.figure(figsize=(15,15))
+        plt.figure(figsize=(12,12))
         ax = rating.value_counts().plot(kind='bar')
         counts = ax.get_yticks()  
         tot = sum(counts)
@@ -148,7 +148,7 @@ class overview_plot:
             value_column = ['Avgscore', 'Cleanliness', 'Comfort', 'Facilities', 'Free Wifi', 'Location', 'Staff', 'Value for money']
             df_mean = df[value_column].groupby(df['category']).mean().T
 
-            plt.figure(figsize=(15,15))
+            plt.figure(figsize=(10,15))
             df_mean.plot(kind = 'bar', alpha = 0.7)
             plt.legend(bbox_to_anchor=(0.85, 1.05), loc=2, borderaxespad=0.)
             plt.xlabel('Rating Category')
@@ -168,7 +168,7 @@ class overview_plot:
             value_column = ['Total_review', 'Avgscore']
             df_mean = df[value_column].groupby(df['ctg']).mean()  
 
-            plt.figure(figsize=(15,15))
+            plt.figure(figsize=(10,15))
             f, axarr = plt.subplots(2, sharex=True)
 
             ind = np.arange(12)
